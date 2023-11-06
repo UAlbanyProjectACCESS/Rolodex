1ba5c3aa82ee33d53790b2343044df578eae21ed
EwokOzwok <eeozmat@buffalo.edu>
2023-11-06 14:10:25 -0500

    new load test done

--- loadtest script.R
+++ loadtest script.R
@@ -1,9 +1,9 @@
 shinyloadtest::record_session('http://localhost:3838/')
 
 #Run in Terminal
-# shinycannon recording.log http://localhost:3838/ --workers 10 --loaded-duration-minutes 5 --output-dir run1
+# shinycannon recording.log http://localhost:3838/ --workers 10 --loaded-duration-minutes 2 --output-dir run1
 # shinycannon recording.log http://localhost:3838/ --workers 10 --loaded-duration-minutes 2 --output-dir run10
-df <- shinyloadtest::load_runs("run10")
-shinyloadtest::shinyloadtest_report(df, "run10.html")
+df <- shinyloadtest::load_runs("run1")
+shinyloadtest::shinyloadtest_report(df, "run1.html")
 
 profvis::profvis(print(Rolodex::run_app()))
