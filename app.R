75b3e33d7828d826210372380274ee08110a7602
EwokOzwok <eeozmat@buffalo.edu>
2024-01-10 12:05:38 -0500

    new deploy with prob gamb resources added

--- app.R
+++ app.R
@@ -3,5 +3,5 @@
 # Or use the blue button on top of this file
 
 pkgload::load_all(export_all = F,helpers = F,attach_testthat = FALSE)
-options( "golem.app.prod" = TRUE)
+options("golem.app.prod" = TRUE)
 Rolodex::run_app() # add parameters here (if any)
