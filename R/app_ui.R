ae98714eec83063cc3c0977e755d71f62c7d32d7
EwokOzwok <eozmat@albany.edu>
2024-12-26 15:35:45 -0500

    attempt at toast moving again...

--- R/app_ui.R
+++ R/app_ui.R
@@ -533,12 +533,12 @@ golem_add_external_resources <- function() {
 
     # Fix Install button to Upper right of screen
     tags$style("
-        .toast-content {
+    button#install-button {
             position: fixed !important;
             top: 20px !important;
             right: 20px !important;
             z-index: 9999 !important;
-        }
+    }
     "),
     # includeCSS("www/framework7.bundle.min.css")
     # bundle_resources(
