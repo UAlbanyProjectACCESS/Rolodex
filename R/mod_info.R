181f356232d63ef812fe19252e6501790ec8171b
EwokOzwok <eozmat@albany.edu>
2025-01-07 13:16:36 -0500

    fixed as.data.frame() issue with rolodex data

--- R/mod_info.R
+++ R/mod_info.R
@@ -101,6 +101,82 @@ mod_info_server <- function(id, selector, data, rownametitle, phone, website){
     })
   })
 }
+# mod_info_server <- function(id, selector, data, rownametitle, phone, website){
+#   moduleServer( id, function(input, output, session){
+#     ns1 <- session$ns
+#
+#
+#     output$info2<- renderText({
+#
+#
+#
+#
+#
+#       text<-c(data[data==rownametitle,2])
+#       text<-na.omit(text)
+#       text
+#
+#
+#
+#     })
+#
+#     if(phone==F){
+#       output$info3<- renderUI({})
+#     } else {
+#       output$info3<- renderUI({
+#
+#
+#
+#
+#         text<-c(data[data==rownametitle,3])
+#         text<-na.omit(text)
+#         f7Link("Call now", href=text)
+#
+#
+#
+#       })
+#     }
+#     if(website==F){
+#       output$info4<- renderUI({})
+#     } else {
+#
+#       output$info4<- renderUI({
+#
+#
+#
+#         text<-c(data[data==rownametitle,4])
+#         text<-na.omit(text)
+#         f7Link(text, href=text)
+#
+#
+#
+#       })
+#     }
+#
+#     output$info5<- renderText({
+#
+#
+#
+#       text<-c(data[data==rownametitle,5])
+#       text<-na.omit(text)
+#       text
+#
+#
+#
+#     })
+#
+#     output$info9<- renderText({
+#
+#
+#       text<-c(data[data==rownametitle,9])
+#       text<-na.omit(text)
+#       text
+#
+#
+#
+#     })
+#   })
+# }
 
 ## To be copied in the UI
 # mod_info_ui("info_1")
--- data-raw/data.R
+++ data-raw/data.R
@@ -1,3 +1,4 @@
 ## code to prepare `data` dataset goes here
 data<-readxl::read_excel("data-raw/Rolodex.xlsx")
+data<-as.data.frame(data)
 usethis::use_data(data, overwrite = TRUE)
